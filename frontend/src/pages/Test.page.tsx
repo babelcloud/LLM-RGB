0d9af0c27681f5bfd67b0e45e63ae54335ad9e68
FLYING <fly88oj@163.com>
2023-11-17 14:34:35 +0800

    Fix test progress

--- frontend/src/pages/Test.page.tsx
+++ frontend/src/pages/Test.page.tsx
@@ -293,7 +293,12 @@ export function TestPage() {
           Test Console
         </Text>
         <Box pos="relative">
-          <Modal opened={running} onClose={closeRunning} withCloseButton={false}>
+          <Modal
+            opened={running}
+            onClose={closeRunning}
+            withCloseButton={false}
+            closeOnClickOutside={false}
+          >
             <Progress value={progress} />
           </Modal>
           <Grid gutter="16" mt={40}>
