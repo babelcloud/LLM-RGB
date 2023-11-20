3582fffddadc866b3c6fba8406250566e609a199
Jeffrey Au <fly88oj@gmail.com>
2023-11-20 17:07:25 +0800

    Fix build issue (#17)

---
+++ frontend/src/client.d.ts
@@ -0,0 +1,6 @@
+declare module '@TestCaseData' {
+    const ConfigMap: Map<string, string>;
+    const PromptMap: Map<string, string>;
+    const AssertMap: Map<string, string>;
+    export { ConfigMap, PromptMap, AssertMap }
+}
