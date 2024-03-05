620a82c653e17ac9855036a3996b82081cc31f9b
Hailong Zhang <zhlmmc@gmail.com>
2024-03-05 16:32:25 +0800

    update models and revise test cases

--- testcases/015_simple_mahjong_assert.js
+++ testcases/015_simple_mahjong_assert.js
@@ -1,21 +1,28 @@
 module.exports = (output, { vars }) => {
     output = output.toLowerCase();
+    const PATTERN_TARGET = "target winning pattern: mixed-win";
     const C1_PATTERN_ACTION = "action: discard c1";
-    const C1_PATTERN_TARGET = "target winning pattern: mixed-win";
     const C1_PATTERN_WAITING = "winning tile(s): b8\n";
 
     const B8_PATTERN_ACTION = "action: discard b8";
-    const B8_PATTERN_TARGET = "target winning pattern: mixed-win";
     const B8_PATTERN_WAITING = "winning tile(s): c1\n";
 
-    if (output.includes(C1_PATTERN_ACTION) && output.includes(C1_PATTERN_TARGET) && output.includes(C1_PATTERN_WAITING)) {
-        return 1;
+    var score = 0;
+    if (output.includes(PATTERN_TARGET)){
+        score += 0.3;
+        if(output.includes(C1_PATTERN_ACTION)){
+            score += 0.2;
+            if(output.includes(C1_PATTERN_WAITING)){
+                score += 0.5;
+            }
+        }
+        else if(output.includes(B8_PATTERN_ACTION)){
+            score += 0.1;
+            if(output.includes(B8_PATTERN_WAITING)){
+                score += 0.3;
+            }
+        }
     }
-    if (output.includes(B8_PATTERN_ACTION) && output.includes(B8_PATTERN_TARGET) && output.includes(B8_PATTERN_WAITING)) {
-        return 0.6;
-    }
-    if (output.includes(C1_PATTERN_TARGET)){
-        return 0.2;
-    }
-    return 0;
+
+    return score;
 };
