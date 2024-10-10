1c43d9c867a314fc18e74ea1cae4b0e09e4c5982
hwoo <hui.hwoo@gmail.com>
2024-10-10 00:39:11 -0700

    Add find_target_files testcase

---
+++ testcases/014_find_target_files_assert.js
@@ -0,0 +1,29 @@
+module.exports = (output, { vars }) => {
+  try {
+      const json = JSON.parse(
+          String(output)
+              .replace(/^```json\n/, "") // Remove the prefix
+              .replace(/\n```$/, "") // Remove the suffix
+      );
+      const { targetFiles } = json;
+
+      // check if targetFiles is an array
+      if (!Array.isArray(targetFiles)) {
+          return false;
+      }
+      const outputSet = new Set(targetFiles);
+      if (outputSet.size !== expectedOutput.size) {
+          return false;
+      }
+
+      // check if targetFiles contains the expected output
+      for (const file of expectedOutput) {
+          if (!outputSet.has(file)) {
+              return false;
+          }
+      }
+      return true;
+  } catch (error) {
+      return false;
+  }
+};
